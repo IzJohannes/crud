e2734e01ed36a0336030c0e7c9e4ac947d148865
IzJohannes <johannes.villasante@upeu.edu.pe>
2022-06-16 21:14:04 -0500

    Examendeobjetos

---
+++ src/practicagui/PracticaGUI.java
@@ -0,0 +1,12 @@
+package practicagui;
+import gui.FormularioPersonasCRUD;
+
+public class PracticaGUI {
+
+    public static void main(String[] args) {
+     FormularioPersonasCRUD jdk = new FormularioPersonasCRUD();  
+     jdk.show();
+     
+    }
+    
+}
